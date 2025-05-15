a40cf98532a670bee672792cbc310891a4901002
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-15 18:49:27 -0300

    ✨  - Criando game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Card from "@/components/Card";
 import ScoreBoard from "./components/ScoreBoard";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4">
       <ScoreBoard moves={0} time={0} onRestart={() => {}} />
-      <Card />
+      <GameBoard />
     </div>
   );
 }
