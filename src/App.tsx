1a8366140c0e545c57b3bf4e822878ed664dc9e9
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-15 18:41:42 -0300

    ✨  - Criando ScoreBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Card from "@/components/Card";
+import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-200">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4">
+      <ScoreBoard moves={0} time={0} onRestart={() => {}} />
       <Card />
     </div>
   );
