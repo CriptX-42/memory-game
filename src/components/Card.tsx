26c10c8c7c3caf23a55be02f5cccb82c622be483
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-14 21:16:32 -0300

    Refatorando classes para usar clsx e tailwind merge

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,39 @@
-import { Sparkles } from "lucide-react";
 import { useState } from "react";
+import { Sparkles } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const CARD_STYLES = {
+  base: "absolute flex h-full w-full items-center justify-center rounded-xl border-2 backface-hidden",
+  back: "border-white/20 bg-pink",
+  front: "rotate-y-180 border-purple-200 bg-white",
+};
 
 function Card() {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
-    setIsFlipped(true);
+    setIsFlipped(!isFlipped);
   };
 
   return (
     <div className="relative h-16 w-16 cursor-pointer" onClick={handleClick}>
-      {/* //*div em 3D */}
+      {/* div do 3D */}
       <div
-        className={`preserve-3d h-full w-full transition-transform duration-500 ${isFlipped && "rotate-y-180"}`}
+        className={cn(
+          "preserve-3d h-full w-full transition-transform duration-500",
+          isFlipped && "rotate-y-180",
+        )}
       >
-        {/* //*BACK */}
-
-        <div className="backface-hidden absolute flex h-full w-full items-center justify-center rounded-xl border-2 border-white/20 bg-pink">
+        {/* Card back */}
+        <div className={cn(CARD_STYLES.base, CARD_STYLES.back)}>
           <Sparkles className="h-6 w-6 text-white" />
         </div>
-        {/* //*FRONT */}
-        <div className="backface-hidden rotate-y-180 absolute flex h-full w-full items-center justify-center rounded-xl border-2 border-purple-200 bg-white">
-          🍬
-        </div>
+
+        {/* Card front */}
+        <div className={cn(CARD_STYLES.base, CARD_STYLES.front)}>🍬</div>
       </div>
     </div>
   );
 }
+
 export default Card;
