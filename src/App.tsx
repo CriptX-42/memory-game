81ac03a6da3bdad8fbffbb5d118bd347e74bc26d
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-17 15:43:15 -0300

    ✨  - Add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import ScoreBoard from "./components/ScoreBoard";
 import GameBoard from "./components/GameBoard";
+import GameModal from "./components/GameModal";
 
 function App() {
+  const gameCompleted = false;
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4">
       <ScoreBoard moves={0} time={0} onRestart={() => {}} />
       <GameBoard />
+      {gameCompleted && <GameModal moves={10} time={0} onRestart={() => {}} />}
     </div>
   );
 }
