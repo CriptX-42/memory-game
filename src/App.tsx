873b1d786ef26662dbe2fb208b68231ee1712a38
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-17 16:36:59 -0300

    ✨  - Criando tela de DifficultySelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ScoreBoard from "./components/ScoreBoard";
 import GameBoard from "./components/GameBoard";
 import GameModal from "./components/GameModal";
+import DifficultySelecotor from "./components/DifficultySelector";
 
 function App() {
   const gameCompleted = true;
+  const difficulty = null;
+
+  if (!difficulty) return <DifficultySelecotor />;
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4 sm:gap-8 sm:p-8">
       <ScoreBoard moves={0} time={0} onRestart={() => {}} />
