13574be3cc350a5833f57a6a216a0c56ca979aa3
Ricardo Carvalho <ricardo-svc@live.com>
2025-07-24 18:27:05 -0300

    WIP: Criando a dificuldade

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import GameModal from "./components/GameModal";
 import DifficultySelecotor from "./components/DifficultySelector";
 import { useMemoryGame } from "./hooks/use-memory-game";
 import { formatTime } from "./lib/formatTime";
+import { Difficulty } from "./types";
+import { useState } from "react";
 
 function App() {
-  const difficulty = "easy";
   const { cards, handleCardClick, moves, time, gameCompleted, resetGame } =
     useMemoryGame();
+  const [difficulty, setdifficulty] = useState<Difficulty | null>();
 
   const formattedTime = formatTime(time);
 
-  if (!difficulty) return <DifficultySelecotor />;
+  if (!difficulty) return <DifficultySelecotor onSelect={difficulty} />;
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-blue-200 p-4 sm:gap-8 sm:p-8">
